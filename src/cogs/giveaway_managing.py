b27b32fe0e11d11cdf40fba2341bd66588e038a0
schlopp <god.tcs@gmail.com>
2022-01-11 18:08:44 +0100

    Update prototype

--- src/cogs/giveaway_managing.py
+++ src/cogs/giveaway_managing.py
@@ -1,11 +1,15 @@
 import asyncio
 import random
+from datetime import datetime, timedelta
 
 import discord  # type: ignore
+from discord import utils as discord_utils  # type: ignore
 from discord.ext import commands, vbu  # type: ignore
 
+import utils
 
-class PingCommand(vbu.Cog):
+
+class GiveawayManaging(vbu.Cog):
     @commands.command(name="create-giveaway", help="Create a new giveaway!")
     @commands.is_slash_command()
     async def _create_giveaway_command(self, ctx: vbu.SlashContext) -> None:
@@ -22,9 +26,25 @@ class PingCommand(vbu.Cog):
         None
         """
 
-        await ctx.interaction.response.send_message("Test!")
+        await ctx.interaction.response.send_message("Creating a testing giveaway!")
+
+        # Create a testing giveaway.
+        giveaway_reward = "1x Classic Nitro"
+        giveaway_ends_at = datetime.utcnow() + timedelta(hours=1)
+        giveaway_message = await ctx.channel.send(
+            "Proto giveaway message! Ending at"
+            f" {discord_utils.format_dt(giveaway_ends_at, style='F')}!"
+            f" Reward: {giveaway_reward}"
+        )
+        giveaway = utils.Giveaway(
+            ctx.guild.id,
+            ctx.channel.id,
+            giveaway_message.id,
+            giveaway_ends_at,
+            giveaway_reward,
+        )
 
 
 def setup(bot: vbu.Bot):
-    x = PingCommand(bot)
+    x = GiveawayManaging(bot)
     bot.add_cog(x)
